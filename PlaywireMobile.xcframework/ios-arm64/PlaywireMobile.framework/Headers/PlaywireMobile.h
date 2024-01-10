0b64b4238f7e8a1949cef147baabee85882c997f
Silvio Jaureguibehere <silvio.jaureguibehere@gmail.com>
2024-01-10 16:09:23 -0300

    Releases/1.0.0 (#1)
    * version 1.0.0
    
    * change pods repo
    change default subspec
    
    * core not embedded into vast
    
    * core not embedded in vast

---
+++ PlaywireMobile.xcframework/ios-arm64/PlaywireMobile.framework/Headers/PlaywireMobile.h
@@ -0,0 +1,18 @@
+//
+//  PlaywireMobile.h
+//  PlaywireMobile
+//
+//  Created by Silvio Jaureguibehere on 23/08/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlaywireMobile.
+FOUNDATION_EXPORT double PlaywireMobileVersionNumber;
+
+//! Project version string for PlaywireMobile.
+FOUNDATION_EXPORT const unsigned char PlaywireMobileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaywireMobile/PublicHeader.h>
+
+
